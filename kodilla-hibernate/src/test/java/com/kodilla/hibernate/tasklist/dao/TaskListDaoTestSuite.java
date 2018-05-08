884c7152e673992de89d31580ce44a6f146e46af
mmatyjasek <matyjasek.marcin@gmail.com>
2018-05-08 22:20:25 +0200

    Task13.2 final commit

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -1,4 +1,38 @@
 package com.kodilla.hibernate.tasklist.dao;
 
+
+import com.kodilla.hibernate.exception.NotFoundEntityException;
+import com.kodilla.hibernate.tasklist.TaskList;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
 public class TaskListDaoTestSuite {
+
+    private TaskListDao taskListDao;
+    private static final String DESCRIPTION = "Lists contains completed tasks";
+    private static final String NAME = "DONE";
+
+    @Test
+    public void testFindByListName() {
+
+        //Given
+        TaskList taskList = new TaskList(NAME, DESCRIPTION);
+
+        //When
+        taskListDao.save(taskList);
+
+        //Then
+        String listName = taskList.getListName();
+        TaskList readTaskList = taskListDao.findByListName(listName).get(0);
+        Assert.assertEquals(listName, readTaskList.getListName());
+
+        //CleanUp
+        taskListDao.deleteById(readTaskList.getId());
+    }
+
 }
