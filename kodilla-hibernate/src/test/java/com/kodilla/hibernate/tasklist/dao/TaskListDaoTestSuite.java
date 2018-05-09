9026c87a05a9cd669e63770bcf948062d8e2835d
“mmatyjasek <matyjasek.marcin@gmail.com>
2018-05-09 12:35:25 +0200

    Task 13.2 correction

--- kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/tasklist/dao/TaskListDaoTestSuite.java
@@ -5,13 +5,14 @@ import com.kodilla.hibernate.tasklist.TaskList;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class TaskListDaoTestSuite {
-
+    @Autowired
     private TaskListDao taskListDao;
     private static final String DESCRIPTION = "Lists contains completed tasks";
     private static final String NAME = "DONE";
